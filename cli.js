c385aa40972ded859cca95d2a797fbf998288449
vanesnery <nery_van@hotmail.com>
2019-12-03 16:43:32 -0300

    inserido reject para os links, alterando caminho dos arquivos testes e teste atualizado

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 const mdLinks = require('./lib/index.js');
+const path = process.argv[2];
+// const option = process.argv[3];
 
-mdLinks('./README.md')
+mdLinks(path)
   .then((result) => result.forEach(e => {
     if (e.text.length < 47){
       console.log(e.href,"-",e.text.substring(0,50))
@@ -9,4 +11,4 @@ mdLinks('./README.md')
       console.log(e.href,"-",e.text.substring(0,47) +'...')
     }
   }))
-  .catch((error) => console.log(error))
+  .catch(console.error);
